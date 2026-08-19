6898ef82d6234754e6016165dfa28aef549f8ebc
Jamie Briggs <jamie.briggs@justice.gov.uk>
2026-08-19 15:05:01 +0100

    BC-765 | Added ClientDateOfBirthDateValidator

---
+++ src/main/java/uk/gov/justice/laa/amend/claim/forms/amendments/validators/ClientDateOfBirthDateValidator.java
@@ -0,0 +1,46 @@
+package uk.gov.justice.laa.amend.claim.forms.amendments.validators;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.ZoneId;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import uk.gov.justice.laa.amend.claim.forms.amendments.AmendmentForm;
+import uk.gov.justice.laa.amend.claim.models.ClaimDetails;
+import uk.gov.justice.laa.amend.claim.viewmodels.viewfield.CivilClaimDetailsViewField;
+import uk.gov.justice.laa.amend.claim.viewmodels.viewfield.ClaimViewField;
+import uk.gov.justice.laa.amend.claim.viewmodels.viewfield.MediationClaimDetailsViewField;
+
+@Component
+public class ClientDateOfBirthDateValidator extends AmendmentDateValidator {
+
+  private static final LocalDate OLDEST_DATE_ALLOWED = LocalDate.of(1900, Month.JANUARY, 1);
+
+  public ClientDateOfBirthDateValidator(MessageSource messageSource) {
+    super(messageSource);
+  }
+
+  @Override
+  public boolean appliesTo(ClaimViewField<?> field) {
+    return CivilClaimDetailsViewField.DATE_OF_BIRTH.equals(field) ||
+        MediationClaimDetailsViewField.DATE_OF_BIRTH.equals(field) ||
+        MediationClaimDetailsViewField.CLIENT_2_DATE_OF_BIRTH.equals(field);
+  }
+
+  @Override
+  public void validate(
+      ClaimDetails claim, ClaimViewField<?> field, AmendmentForm form, Errors errors) {
+    var dateOfBirth = form.getDateValue(field.name());
+
+    if(dateOfBirth == null){
+      return;
+    }
+
+    if(dateOfBirth.isBefore(OLDEST_DATE_ALLOWED)){
+      addDateTooEarlyMessage(errors, field, OLDEST_DATE_ALLOWED);
+    } else if(dateOfBirth.isAfter(LocalDate.now(ZoneId.systemDefault()))){
+      addDateInTheFutureMessage(errors, field);
+    }
+  }
+}
---
+++ src/test/java/uk/gov/justice/laa/amend/claim/forms/amendments/validators/ClientDateOfBirthDateValidatorTest.java
@@ -0,0 +1,330 @@
+package uk.gov.justice.laa.amend.claim.forms.amendments.validators;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDate;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+import uk.gov.justice.laa.amend.claim.forms.amendments.AmendmentForm;
+import uk.gov.justice.laa.amend.claim.models.ClaimDetails;
+import uk.gov.justice.laa.amend.claim.resources.MockClaimsFunctions;
+import uk.gov.justice.laa.amend.claim.support.TestMessageSources;
+import uk.gov.justice.laa.amend.claim.viewmodels.viewfield.CivilClaimDetailsViewField;
+import uk.gov.justice.laa.amend.claim.viewmodels.viewfield.MediationClaimDetailsViewField;
+
+class ClientDateOfBirthDateValidatorTest {
+
+  ClientDateOfBirthDateValidator validator;
+
+  @BeforeEach
+  void beforeEach() {
+    validator = new
+        ClientDateOfBirthDateValidator(TestMessageSources.real());
+  }
+
+  @Nested
+  class Civil {
+
+    @Test
+    void testAppliesTo() {
+      assertTrue(validator.appliesTo(CivilClaimDetailsViewField.DATE_OF_BIRTH));
+    }
+
+    @ParameterizedTest
+    @MethodSource(
+        "uk.gov.justice.laa.amend.claim.forms.amendments.validators"
+            + ".InvalidDateArgumentsProvider#invalidDateProvider")
+    void shouldIgnoreUnparseableDateInputs(String day, String month, String year) {
+      Map<String, String> input =
+          Map.of(
+              "DATE_OF_BIRTH-day", day,
+              "DATE_OF_BIRTH-month", month,
+              "DATE_OF_BIRTH-year", year);
+
+      Errors result = validateCivil(input);
+
+      assertThat(result.hasFieldErrors()).isFalse();
+    }
+
+    @Test
+    void shouldNotAddErrorsForValidDate() {
+      Map<String, String> input =
+          Map.of(
+              "DATE_OF_BIRTH-day", "20",
+              "DATE_OF_BIRTH-month", "5",
+              "DATE_OF_BIRTH-year", "2013");
+
+      Errors result = validateCivil(input);
+
+      assertThat(result.hasFieldErrors()).isFalse();
+    }
+
+    @Test
+    void shouldNotAddErrorsForFirstValidDate() {
+      Map<String, String> input =
+          Map.of(
+              "DATE_OF_BIRTH-day", "1",
+              "DATE_OF_BIRTH-month", "1",
+              "DATE_OF_BIRTH-year", "1900");
+
+      Errors result = validateCivil(input);
+
+      assertThat(result.hasFieldErrors()).isFalse();
+    }
+
+    @Test
+    void shouldAddErrorsForOldDate() {
+      Map<String, String> input =
+          Map.of(
+              "DATE_OF_BIRTH-day", "31",
+              "DATE_OF_BIRTH-month", "12",
+              "DATE_OF_BIRTH-year", "1899");
+
+      Errors result = validateCivil(input);
+
+      assertThat(result.hasFieldErrors()).isTrue();
+      assertThat(result.getFieldError("inputs['DATE_OF_BIRTH']").getCode())
+          .isEqualTo(AmendmentDateValidator.DATE_CANT_BE_BEFORE_CODE);
+      assertThat(result.getFieldError("inputs['DATE_OF_BIRTH']").getArguments()[0])
+          .isEqualTo("Date of birth");
+    }
+
+    @Test
+    void shouldAddErrorsForDateInFuture() {
+      var tomorrow = LocalDate.now().plusDays(1);
+      Map<String, String> input =
+          Map.of(
+              "DATE_OF_BIRTH-day", String.valueOf(tomorrow.getDayOfMonth()),
+              "DATE_OF_BIRTH-month", String.valueOf(tomorrow.getMonthValue()),
+              "DATE_OF_BIRTH-year", String.valueOf(tomorrow.getYear()));
+
+      Errors result = validateCivil(input);
+
+      assertThat(result.hasFieldErrors()).isTrue();
+      assertThat(result.getFieldError("inputs['DATE_OF_BIRTH']").getCode())
+          .isEqualTo(AmendmentDateValidator.DATE_CANT_BE_IN_FUTURE_CODE);
+      assertThat(result.getFieldError("inputs['DATE_OF_BIRTH']").getArguments()[0])
+          .isEqualTo("Date of birth");
+    }
+
+    private Errors validateCivil(Map<String, String> inputs) {
+      ClaimDetails claimDetails = MockClaimsFunctions.createMockCivilClaim();
+
+      var form = new AmendmentForm();
+      form.setInputs(inputs);
+
+      var errors = new BeanPropertyBindingResult(form, "amendmentForm");
+      validator.validate(
+          claimDetails, CivilClaimDetailsViewField.DATE_OF_BIRTH, form, errors);
+      return errors;
+    }
+  }
+
+
+  @Nested
+  class Mediation {
+
+    @Test
+    void testAppliesTo() {
+      assertTrue(validator.appliesTo(MediationClaimDetailsViewField.DATE_OF_BIRTH));
+    }
+
+    @ParameterizedTest
+    @MethodSource(
+        "uk.gov.justice.laa.amend.claim.forms.amendments.validators"
+            + ".InvalidDateArgumentsProvider#invalidDateProvider")
+    void shouldNotAddErrorsForInvalidDates(String day, String month, String year) {
+      Map<String, String> input =
+          Map.of(
+              "DATE_OF_BIRTH-day", day,
+              "DATE_OF_BIRTH-month", month,
+              "DATE_OF_BIRTH-year", year);
+
+      Errors result = validateMediationClient2(input);
+
+      assertThat(result.hasFieldErrors()).isFalse();
+    }
+
+    @Test
+    void shouldNotAddErrorsForValidDate() {
+      Map<String, String> input =
+          Map.of(
+              "DATE_OF_BIRTH-day", "20",
+              "DATE_OF_BIRTH-month", "5",
+              "DATE_OF_BIRTH-year", "2013");
+
+      Errors result = validateMediationClient2(input);
+
+      assertThat(result.hasFieldErrors()).isFalse();
+    }
+
+    @Test
+    void shouldNotAddErrorsForFirstValidDate() {
+      Map<String, String> input =
+          Map.of(
+              "DATE_OF_BIRTH-day", "1",
+              "DATE_OF_BIRTH-month", "1",
+              "DATE_OF_BIRTH-year", "1900");
+
+      Errors result = validateMediationClient2(input);
+
+      assertThat(result.hasFieldErrors()).isFalse();
+    }
+
+    @Test
+    void shouldAddErrorsForOldDate() {
+      Map<String, String> input =
+          Map.of(
+              "DATE_OF_BIRTH-day", "31",
+              "DATE_OF_BIRTH-month", "12",
+              "DATE_OF_BIRTH-year", "1899");
+
+      Errors result = validateMediationClient2(input);
+
+      assertThat(result.hasFieldErrors()).isTrue();
+      assertThat(result.getFieldError("inputs['DATE_OF_BIRTH']").getCode())
+          .isEqualTo(AmendmentDateValidator.DATE_CANT_BE_BEFORE_CODE);
+      assertThat(result.getFieldError("inputs['DATE_OF_BIRTH']").getArguments()[0])
+          .isEqualTo("Date of birth");
+    }
+
+    @Test
+    void shouldAddErrorsForDateInFuture() {
+      var tomorrow = LocalDate.now().plusDays(1);
+      Map<String, String> input =
+          Map.of(
+              "DATE_OF_BIRTH-day", String.valueOf(tomorrow.getDayOfMonth()),
+              "DATE_OF_BIRTH-month", String.valueOf(tomorrow.getMonthValue()),
+              "DATE_OF_BIRTH-year", String.valueOf(tomorrow.getYear()));
+
+      Errors result = validateMediationClient2(input);
+
+      assertThat(result.hasFieldErrors()).isTrue();
+      assertThat(result.getFieldError("inputs['DATE_OF_BIRTH']").getCode())
+          .isEqualTo(AmendmentDateValidator.DATE_CANT_BE_IN_FUTURE_CODE);
+      assertThat(result.getFieldError("inputs['DATE_OF_BIRTH']").getArguments()[0])
+          .isEqualTo("Date of birth");
+    }
+
+    private Errors validateMediationClient2(Map<String, String> inputs) {
+      ClaimDetails claimDetails = MockClaimsFunctions.createMockMediationClaim();
+
+      var form = new AmendmentForm();
+      form.setInputs(inputs);
+
+      var errors = new BeanPropertyBindingResult(form, "amendmentForm");
+      validator.validate(
+          claimDetails, MediationClaimDetailsViewField.DATE_OF_BIRTH, form, errors);
+      return errors;
+    }
+
+
+  }
+
+  @Nested
+  class MediationClient2 {
+
+    @Test
+    void testAppliesTo() {
+      assertTrue(validator.appliesTo(MediationClaimDetailsViewField.CLIENT_2_DATE_OF_BIRTH));
+    }
+
+    @ParameterizedTest
+    @MethodSource(
+        "uk.gov.justice.laa.amend.claim.forms.amendments.validators"
+            + ".InvalidDateArgumentsProvider#invalidDateProvider")
+    void shouldNotAddErrorsForInvalidDates(String day, String month, String year) {
+      Map<String, String> input =
+          Map.of(
+              "CLIENT_2_DATE_OF_BIRTH-day", day,
+              "CLIENT_2_DATE_OF_BIRTH-month", month,
+              "CLIENT_2_DATE_OF_BIRTH-year", year);
+
+      Errors result = validateMediation(input);
+
+      assertThat(result.hasFieldErrors()).isFalse();
+    }
+
+    @Test
+    void shouldNotAddErrorsForValidDate() {
+      Map<String, String> input =
+          Map.of(
+              "CLIENT_2_DATE_OF_BIRTH-day", "20",
+              "CLIENT_2_DATE_OF_BIRTH-month", "5",
+              "CLIENT_2_DATE_OF_BIRTH-year", "2013");
+
+      Errors result = validateMediation(input);
+
+      assertThat(result.hasFieldErrors()).isFalse();
+    }
+
+    @Test
+    void shouldNotAddErrorsForFirstValidDate() {
+      Map<String, String> input =
+          Map.of(
+              "CLIENT_2_DATE_OF_BIRTH-day", "1",
+              "CLIENT_2_DATE_OF_BIRTH-month", "1",
+              "CLIENT_2_DATE_OF_BIRTH-year", "1900");
+
+      Errors result = validateMediation(input);
+
+      assertThat(result.hasFieldErrors()).isFalse();
+    }
+
+    @Test
+    void shouldAddErrorsForOldDate() {
+      Map<String, String> input =
+          Map.of(
+              "CLIENT_2_DATE_OF_BIRTH-day", "31",
+              "CLIENT_2_DATE_OF_BIRTH-month", "12",
+              "CLIENT_2_DATE_OF_BIRTH-year", "1899");
+
+      Errors result = validateMediation(input);
+
+      assertThat(result.hasFieldErrors()).isTrue();
+      assertThat(result.getFieldError("inputs['CLIENT_2_DATE_OF_BIRTH']").getCode())
+          .isEqualTo(AmendmentDateValidator.DATE_CANT_BE_BEFORE_CODE);
+      assertThat(result.getFieldError("inputs['CLIENT_2_DATE_OF_BIRTH']").getArguments()[0])
+          .isEqualTo("Date of birth");
+    }
+
+    @Test
+    void shouldAddErrorsForDateInFuture() {
+      var tomorrow = LocalDate.now().plusDays(1);
+      Map<String, String> input =
+          Map.of(
+              "CLIENT_2_DATE_OF_BIRTH-day", String.valueOf(tomorrow.getDayOfMonth()),
+              "CLIENT_2_DATE_OF_BIRTH-month", String.valueOf(tomorrow.getMonthValue()),
+              "CLIENT_2_DATE_OF_BIRTH-year", String.valueOf(tomorrow.getYear()));
+
+      Errors result = validateMediation(input);
+
+      assertThat(result.hasFieldErrors()).isTrue();
+      assertThat(result.getFieldError("inputs['CLIENT_2_DATE_OF_BIRTH']").getCode())
+          .isEqualTo(AmendmentDateValidator.DATE_CANT_BE_IN_FUTURE_CODE);
+      assertThat(result.getFieldError("inputs['CLIENT_2_DATE_OF_BIRTH']").getArguments()[0])
+          .isEqualTo("Date of birth");
+    }
+
+    private Errors validateMediation(Map<String, String> inputs) {
+      ClaimDetails claimDetails = MockClaimsFunctions.createMockMediationClaim();
+
+      var form = new AmendmentForm();
+      form.setInputs(inputs);
+
+      var errors = new BeanPropertyBindingResult(form, "amendmentForm");
+      validator.validate(
+          claimDetails, MediationClaimDetailsViewField.CLIENT_2_DATE_OF_BIRTH, form, errors);
+      return errors;
+    }
+
+
+  }
+}
